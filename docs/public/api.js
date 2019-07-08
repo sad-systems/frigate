650bed8d4e40052966952aceff85529f8c7bf4f1
MrDigger <mrdigger@mail.ru>
2019-07-08 16:25:44 +0300

    Docs has been added.

---
+++ docs/public/api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "core.scroll.BaseEffects",
+        "core.scroll.Core",
+        "react.ScrollAnimator"
+    ],
+    "modules": [
+        "Core",
+        "React"
+    ],
+    "allModules": [
+        {
+            "displayName": "Core",
+            "name": "Core",
+            "description": "The core module.\n\nProvides the framework independent functions and classes."
+        },
+        {
+            "displayName": "React",
+            "name": "React",
+            "description": "The react module.\n\nProvides the components for React framework."
+        }
+    ],
+    "elements": []
+} };
+});
