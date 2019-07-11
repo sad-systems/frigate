342f2e5ef45b8f2d683078e6d8bcc249a6d89470
MrDigger <mrdigger@mail.ru>
2019-07-11 11:45:52 +0300

    Auto version has been added.

---
+++ copy-version.js
@@ -0,0 +1,20 @@
+/**
+ * Sync project version for all child packages.
+ * Script to auto change project version in all package.json files under the `dist` folder.
+ */
+const getProjectVersion = (require('get-project-version')).default;
+const replace           = require('replace-in-file');
+
+const currentProjectVersion = getProjectVersion({ template: '{{version}}' });
+
+const options = {
+  files: 'dist/**/package.json',
+  from: /"version":\s*"[^"]*"/,
+  to: `"version": "${currentProjectVersion}"`,
+};
+
+const results = replace.sync(options);
+
+console.log('current project version: ', currentProjectVersion);
+console.log('replacement:', results);
+
